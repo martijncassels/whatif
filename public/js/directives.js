9d28418d1ee905e998fd2cb2dd4c874b1c0de374
Martijn Cassels <info@martijncassels.nl>
2017-10-05 09:27:27 +0200

    added loader and ng-cloak 05-10-2017

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,8 +3,8 @@
 /* Directives */
 
 
-angular.module('whatif.directives', []).
-	directive('appVersion', ['version', function(version) {
+angular.module('whatif.directives', [])
+  .directive('appVersion', ['version', function(version) {
     	return function(scope, elm, attrs) {
       		elm.text(version);
     	}
@@ -25,4 +25,31 @@ angular.module('whatif.directives', []).
   //       }
   //     };
   //   }])
+  .directive('loading',   ['$http' ,function ($http)
+    {
+        return {
+            restrict: 'A',
+            link: function (scope, elm, attrs)
+            {
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v)
+                {
+                    // if(v){
+                    //     elm.show();
+                    // }else{
+                    //     elm.hide();
+                    // }
+                    if (v) {
+                            elm.css('display', 'block');
+                        } else {
+                            elm.css('display', 'none');
+                        }
+                });
+            }
+        };
+
+    }])
   ;
